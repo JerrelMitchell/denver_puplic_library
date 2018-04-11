ee22416728dbbdfe9acb7be51495aeb0cba2eb37
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-11 09:35:08 -0600

    add methods to have author tests to pass. iteration 2 complete

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,20 @@
-class Author
+require './lib/book'
 
+class Author
+  attr_reader :attributes, :first_name, :last_name, :books
+  def initialize(attributes)
+    @attributes = (
+      @first_name = attributes[:first_name]
+      @last_name = attributes[:last_name]
+    )
+    @books = []
+  end
+
+  def add_book(title, date)
+    book = Book.new(author_first_name: first_name,
+                    author_last_name: last_name,
+                    title: title,
+                    publication_date: date)
+    @books << book
+  end
 end
