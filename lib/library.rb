25c1354ec3e7da6f9a5c4e55a353424c67aff3f4
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-11 10:50:56 -0600

    add methods for first half of iteration 5

--- lib/library.rb
+++ lib/library.rb
@@ -17,4 +17,16 @@ class Library
   def card_catalgue
     @books.sort_by(&:author_last_name)
   end
+
+  def find_all_with_name(name)
+    @books.find_all do |book|
+      full_name = "#{book.author_first_name} #{book.author_last_name}"
+      full_name.downcase.include?(name.downcase)
+    end
+  end
+
+  def find_by_author(name)
+    found = find_all_with_name(name)
+    found.group_by(&:title)
+  end
 end
